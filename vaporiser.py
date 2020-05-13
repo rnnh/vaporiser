951628ccf00c8dd51aa455a8186beb2fa56da4a6
Ronan Harrington <r.harrington747@gmail.com>
2020-05-13 13:57:32 +0100

    Updated Python requirements, added main script

---
+++ vaporiser.py
@@ -0,0 +1,100 @@
+#!/usr/bin/env python
+
+# Loading modules
+from pysndfx import AudioEffectsChain
+import moviepy.editor as movedit
+import argparse
+import datetime
+import sys
+import re
+
+# Parsing for command line arguments
+parser = argparse.ArgumentParser(
+    formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+    description = "Creates a vaporwave (slowed and reverb) remix of a given MP3\
+    file, with the option of playing over a looped GIF as a video.")
+
+parser.add_argument("-g", "--gif",
+                    dest = "gif_file",
+                    help = "Input GIF file to loop. Without a GIF, only an MP3 \
+                    is created. With a GIF, an MP4 video is also created.",
+                    type = str)
+
+parser.add_argument("-o", "--output",
+                    dest = "output_name",
+                    help = "Name of output file.",
+                    type = str)
+
+parser.add_argument("-s", "--speed",
+                    dest = "speed_ratio",
+                    help = "Ratio of new playback speed to old speed.",
+                    type = float,
+                    default = 0.85)
+
+parser.add_argument("-p", "--pitch",
+                    dest = "pitch_shift",
+                    help = "Pitch shift (100ths of a semitone).",
+                    type = float,
+                    default = -50)
+
+parser.add_argument("-l", "--lowpass",
+                    dest = "lowpass_cutoff",
+                    help = "Cutoff for lowpass filter (Hz).",
+                    type = int,
+                    default = 3500)
+
+required_arguments = parser.add_argument_group("required arguments")
+
+required_arguments.add_argument("-a", "--audio",
+                                dest = "audio_input",
+                                help = "Input audio file to vaporise (.mp3)",
+                                type = str,
+                                required = True)
+
+args = parser.parse_args()
+
+# Setting name of output file
+if args.output_name is None:
+    # If no output name is given, add "_vaporised" to input audio file name
+    audio_input_string = re.sub(".mp3", "", str(args.audio_input))
+    audio_output = audio_input_string + "_vaporised.mp3"
+    video_output = audio_input_string + "_vaporised.mp4"
+else:
+    # Otherwise, use the output file name given via the command line
+    output_string = re.sub(".mp3", "", str(args.output_name))
+    output_string = re.sub(".mp4", "", str(output_string))
+    audio_output = output_string + ".mp3"
+    video_output = output_string + ".mp4"
+    if args.audio_input == args.output_name:
+        print("ERROR: Input and output name are identical")
+        sys.exit()
+
+# Creating audio effects chain function
+fx = (
+    AudioEffectsChain()
+    .reverb()
+    .lowpass(args.lowpass_cutoff)
+    .speed(args.speed_ratio)
+    .pitch(args.pitch_shift)
+)
+
+# Applying audio effects
+fx(args.audio_input, audio_output)
+
+# Create video if a GIF file is provided
+if args.gif_file is None:
+    # If no GIF is provided, exit here
+    print("Script finished at", datetime.datetime.now().strftime('%H:%M:%S'))
+    print("Vaporised MP3 file (audio):", audio_output)
+    sys.exit()
+else:
+    # If a GIF is provided, loop it for the length of the vaporised audio file
+    mp3_movedit = movedit.AudioFileClip(audio_output)
+    gif_movedit = movedit.VideoFileClip(args.gif_file)
+    number_of_loops = float(mp3_movedit.duration / gif_movedit.duration)
+    gif_looped = gif_movedit.loop(number_of_loops)
+    gif_looped_with_audio = gif_looped.set_audio(mp3_movedit)
+    gif_looped_with_audio.write_videofile(video_output)
+    print("Script finished at", datetime.datetime.now().strftime('%H:%M:%S'))
+    print("Vaporised MP3 file (audio):", audio_output)
+    print("Vaporised MP4 file (video):", video_output)
